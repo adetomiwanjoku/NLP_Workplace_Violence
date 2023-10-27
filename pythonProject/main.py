19ee107fb980b4f24c3976c0a26ed66869737439
ChinweNjoku <tomiwa.njoku@gmail.com>
2023-10-27 15:50:55 +0100

    made changes

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,36 +1,59 @@
+from sentence_transformers import SentenceTransformer, util
 import pandas as pd
-import spacy
+import nltk
+from gensim.parsing.preprocessing import remove_stopwords
 
-# Load spaCy NLP model
-nlp = spacy.load('en_core_web_sm')
+# Load the SentenceTransformer model
+model = SentenceTransformer('stsb-roberta-large')
 
-# Load data from Excel sheets into pandas DataFrames
-df1 = pd.read_csv(r'C:\Users\ChinweNjoku\PycharmProjects\pythonProject\20231010_EIRFsampleData.csv', encoding = 'latin1')
-df2 = pd.read_csv(r'C:\Users\ChinweNjoku\PycharmProjects\pythonProject\20231010_WAASBsampleData.csv', encoding = 'latin1')
+# Load data from Excel sheets into pandas DataFrames, considering only the 'Description' column
+df1 = pd.read_csv(r'C:\Users\ChinweNjoku\PycharmProjects\pythonProject\20231010_EIRFsampleData.csv', usecols=['DESCRIPTION'],
+                    encoding='latin1')  # Specify the correct encoding (e.g., 'latin1')
+df2 = pd.read_csv(r'C:\Users\ChinweNjoku\PycharmProjects\pythonProject\20231010_WAASBsampleData.csv', usecols=['DESCRIPTION'],
+                    encoding='latin1')  # Specify the correct encoding (e.g., 'latin1')
 
+# Download NLTK resources
+nltk.download('punkt')
 
-# Define a function to calculate semantic similarity
-def calculate_similarity(text1, text2):
-    doc1 = nlp(text1.lower())
-    doc2 = nlp(text2.lower())
-    return doc1.similarity(doc2)
+# Preprocess text data
+def preprocess_text(text):
+    # Handle NaN values by replacing them with an empty string
+    text = str(text) if not pd.isnull(text) else ''
+    # Lowercase the text
+    text = text.lower()
+    # Tokenize the text
+    tokens = nltk.word_tokenize(text)
+    # Remove stopwords
+    tokens = [word for word in tokens if word.isalnum() and word not in remove_stopwords(text)]
+    # Join tokens back into a cleaned text
+    cleaned_text = ' '.join(tokens)
+    return cleaned_text
 
+# Compute sentence embeddings for the first DataFrame after text preprocessing
+embeddings1 = model.encode(df1['DESCRIPTION'].apply(preprocess_text).astype(str).tolist(), convert_to_tensor=True)
 
-# Iterate through rows and identify duplicates
-for index1, row1 in df1.iterrows():
-    # Iterate through rows and identify duplicates based on the 'Description' column
-    for index1, row1 in df1.iterrows():
-        for index2, row2 in df2.iterrows():
-            similarity_score = calculate_similarity(str(row1['DESCRIPTION']), str(row2['DESCRIPTION']))
+# Compute sentence embeddings for the second DataFrame after text preprocessing
+embeddings2 = model.encode(df2['DESCRIPTION'].apply(preprocess_text).astype(str).tolist(), convert_to_tensor=True)
 
-            # If similarity score is above a threshold, consider them duplicates
-            similarity_threshold = 0.9
-            if similarity_score > similarity_threshold:
-                print(
-                    f'Duplicate Found: Row {index1 + 1} in Sheet 1 and Row {index2 + 1} in Sheet 2, Similarity Score: {similarity_score:.2f}')
+# List to store duplicates
+duplicates = []
 
+# Iterate through rows and identify duplicates based on cosine similarity
+for index1, embedding1 in enumerate(embeddings1):
+    for index2, embedding2 in enumerate(embeddings2):
+        # Calculate cosine similarity between embeddings
+        similarity_score = util.pytorch_cos_sim(embedding1, embedding2)[0][0].item()
 
+        # If similarity score is above a threshold, consider them duplicates
+        similarity_threshold = 0.8
+        if similarity_score > similarity_threshold:
+            duplicates.append((index1 + 1, index2 + 1, similarity_score))  # Store duplicates
 
+# Create a DataFrame from the duplicates list
+duplicates_df = pd.DataFrame(duplicates, columns=['Sheet 1 Row', 'Sheet 2 Row', 'Similarity Score'])
 
+# Save the duplicates to a CSV file
+duplicates_df.to_csv('duplicates.csv', index=False)
 
+print("Duplicates saved to 'duplicates.csv'")
 
