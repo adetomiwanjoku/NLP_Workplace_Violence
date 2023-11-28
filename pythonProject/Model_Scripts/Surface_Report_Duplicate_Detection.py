acb273b75546f259d98054561f552c003e8638bd
adetomiwanjoku <adetomiwanjoku>
2023-11-28 09:57:20 +0000

    tidying the notebook

--- pythonProject/Model_Scripts/Surface_Report_Duplicate_Detection.py
+++ pythonProject/Model_Scripts/Surface_Report_Duplicate_Detection.py
@@ -1,4 +1,13 @@
 # Databricks notebook source
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Install Packages 
+
+# COMMAND ----------
+
 # MAGIC %pip install sentence-transformers
 # MAGIC %pip install pandas
 # MAGIC %pip install nltk 
@@ -7,6 +16,11 @@
 # MAGIC %pip install torch
 # MAGIC %pip install numpy 
 # MAGIC %pip install matplotlib 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Import Packages 
 
 # COMMAND ----------
 
@@ -27,6 +41,11 @@ from sklearn.decomposition import PCA
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # Read in the data
+
+# COMMAND ----------
+
 df1 = pd.read_csv('/Workspace/Repos/adetomiwanjoku@tfl.gov.uk/NLP_Workplace_Violence/pythonProject/Data/three_day_copy_surface_reports.csv', encoding = 'latin1')
 
 # COMMAND ----------
@@ -43,13 +62,22 @@ df1 = df1.rename(columns={'Location / Road Name': 'Location'})
 
 # COMMAND ----------
 
-
 df1['Location'] = df1['Location'].str.title() # Useful as this station names are written each word is capatalised
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Load model and tokenizer
 
 # COMMAND ----------
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
 model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Text Preprocessing 
 
 # COMMAND ----------
 
@@ -85,11 +113,23 @@ sentences_from_file1 = [clean_text(sentence) for sentence in df1['Description'].
 
 # COMMAND ----------
 
+# Embed the words 
+
+# COMMAND ----------
+
 # Encode sentences to obtain embeddings
 embeddings1 = model.encode(sentences_from_file1, convert_to_tensor=True)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # Define cosine similarity 
+
+# COMMAND ----------
+
+
+# Assuming you have a list of locations for each sentence
+locations_from_file1 = [...]  # Replace [...] with your actual list of locations
 
 # Reset the index of df1
 df1 = df1.reset_index(drop=True)
@@ -105,7 +145,7 @@ similar_reports_indices = [
     (i, j)
     for i in range(len(sentences_from_file1))
     for j in range(i + 1, len(sentences_from_file1))
-    if (similarity_matrix[i, j] > threshold)
+    if (similarity_matrix[i, j] > threshold) and ('BLANK' not in [sentences_from_file1[i], sentences_from_file1[j]])
 ]
 
 # Create a DataFrame for similar reports
@@ -113,10 +153,10 @@ similar_reports_df = pd.DataFrame([
     {
         'File1_Index': i,
         'File2_Index': j,
-        'Description': sentences_from_file1[i],
-        'Duplicate': sentences_from_file1[j],
+        'Description': sentences_from_file1[i] if sentences_from_file1[i] != 'BLANK' else sentences_from_file1[j],
+        'Duplicate': sentences_from_file1[j] if sentences_from_file1[j] != 'BLANK' else sentences_from_file1[i],
+        'Location': locations_from_file1[i],  # Add the Location column
         'Similarity_Score': similarity_matrix[i, j],
-        
     }
     for i, j in similar_reports_indices
 ])
@@ -124,6 +164,7 @@ similar_reports_df = pd.DataFrame([
 # Add new index columns for each file index that adds two to the existing index
 similar_reports_df['Row_Num'] = similar_reports_df['File1_Index'] + 2
 similar_reports_df['Row_Num_Duplicate'] = similar_reports_df['File2_Index'] + 2
+2
 
 
 # COMMAND ----------
@@ -137,9 +178,29 @@ similar_reports_df['Is_Duplicate'] = ''
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # Create a data frame output 
+
+# COMMAND ----------
+
 # Reorder columns with the new index as the first column
 similar_reports_df = similar_reports_df[['Row_Num', 'Row_Num_Duplicate', 'Description', 'Duplicate','Similarity_Score_Percent', 'Is_Duplicate']]
 
 # COMMAND ----------
 
 display(similar_reports_df)
+
+# COMMAND ----------
+
+
+description_column = df1['Description']
+
+# Check if the values are equal to 'blank'
+is_equal_to_blank = description_column == '[BLANK]'
+
+# Count the number of values equal to 'blank'
+count_equal_to_blank = is_equal_to_blank.sum()
+
+print(f"Number of descriptions equal to 'blank': {count_equal_to_blank}")
+
+
