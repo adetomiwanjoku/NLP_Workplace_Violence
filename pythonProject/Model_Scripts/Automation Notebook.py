818de9598fd26bc280885510c0baf13743dca12e
adetomiwanjoku <adetomiwanjoku>
2024-02-05 10:23:04 +0000

    Deleted the competency notebook and tweaking the automation notebook #

--- pythonProject/Model_Scripts/Automation Notebook.py
+++ pythonProject/Model_Scripts/Automation Notebook.py
@@ -7,10 +7,47 @@ import base64
 # COMMAND ----------
 
 # Your Logic App HTTP trigger URL
-url ="https://prod-08.northeurope.logic.azure.com:443/workflows/0613a91a9f66435a9c0d3f96ba989a61/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6slbcxFusde4k0aa9J4ccZBb-bA6PzzF3zXrtHCKFyw"
+url = "https://prod-08.northeurope.logic.azure.com:443/workflows/0613a91a9f66435a9c0d3f96ba989a61/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6slbcxFusde4k0aa9J4ccZBb-bA6PzzF3zXrtHCKFyw"
+
+
+# COMMAND ----------
 
 # Read your CSV file and encode it in base64
-with open('trial.csv', 'rb') as file:
+with open("/Workspace/Repos/adetomiwanjoku@tfl.gov.uk/NLP_Workplace_Violence/pythonProject/Model_Scripts/trial.csv", "rb") as file:
+    csv_content_base64 = base64.b64encode(file.read()).decode("utf-8")
+
+
+# COMMAND ----------
+
+
+# Prepare the JSON payload
+data = {
+    "receiver": ["AdetomiwaNjoku@tfl.gov.uk", "AdetomiwaNjoku@tfl.gov.uk"],
+    "subject": "Your Email Subject",
+    "message": "Your email body message",
+    }
+
+
+
+# COMMAND ----------
+
+# Send the POST request
+response = requests.post(url, json=data)
+
+# Print the status code of the response
+print(response.status_code)
+
+# COMMAND ----------
+
+import requests
+import json
+import base64
+
+# Your Logic App HTTP trigger URL
+url = 'https://prod-08.northeurope.logic.azure.com:443/workflows/0613a91a9f66435a9c0d3f96ba989a61/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6slbcxFusde4k0aa9J4ccZBb-bA6PzzF3zXrtHCKFyw'
+
+# Read your CSV file and encode it in base64
+with open("/Workspace/Repos/adetomiwanjoku@tfl.gov.uk/NLP_Workplace_Violence/pythonProject/Model_Scripts/trial.csv", 'rb') as file:
     csv_content_base64 = base64.b64encode(file.read()).decode('utf-8')
 
 # Prepare the JSON payload
@@ -24,4 +61,8 @@ data = {
 # Send the POST request
 response = requests.post(url, json=data)
 
-print(response.status_code)
+# Check response
+if response.status_code == 200:
+    print("Data sent successfully")
+else:
+    print("Failed to send data")
